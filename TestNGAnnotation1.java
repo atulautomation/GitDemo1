6784a807587fb6ceacd6722e76cfe936b61604c5
Atul <atulingle005@gmail.com>
2022-08-22 16:59:35 +0530

    First code

---
+++ TestNGAnnotation1.java
@@ -0,0 +1,28 @@
+package pkg_SeleniumPractice;
+
+import org.testng.annotations.Test;
+
+public class TestNGAnnotation1 {
+
+	@Test
+	public void TC_01()
+	{
+		System.out.println("This is first selenium test");
+	}
+	
+	@Test
+	public void TC_02()
+	{
+		System.out.println("This is second selenium test");
+	}
+@Test
+	public void TC_03()
+	{
+		System.out.println("This is Third selenium test");
+@Test
+	public void TC_04()
+	{
+		System.out.println("This is fourth selenium test");
+	
+	
+}
---
+++ TestNGAnnotation2.java
@@ -0,0 +1,32 @@
+package pkg_SeleniumPractice;
+
+import org.testng.annotations.Test;
+
+public class TestNGAnnotation2 {
+
+	@Test
+	public void TC_01()
+	{
+		System.out.println("This is first RESTAPI test");
+	}
+	
+	@Test
+	public void TC_02()
+	{
+		System.out.println("This is second RESTAPI test");
+	}
+	
+	@Test
+	public void TC_03()
+	{
+		System.out.println("This is third RESTAPI test");
+	}
+<<<<<<< HEAD:src/pkg_SeleniumPractice/TestNGAnnotation2.java
+@Test
+	public void TC_04()
+	{
+		System.out.println("This is fourth RESTAPI test");
+	}
+=======
+>>>>>>> 82112b0095bebaed7f65c90f02fcc715262703e6:SeleniumProject1/src/pkg_SeleniumPractice/TestNGAnnotation2.java
+}
